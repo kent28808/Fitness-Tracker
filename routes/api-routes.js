7a2441ac4c37d3966c2a32afa9d43d79b01690d4
kent28808 <kktanoue@gmail.com>
2020-11-06 00:14:58 -0800

    updated html routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,19 +2,29 @@ const express = require("express");
 const router = express.Router();
 const db = require("../models")
 
-router.get("/api/workouts",function(req,res){
-db.Workout.find().then(dbresults => {
-    res.json(dbresults)
-})
+//Get last workout
+router.get("/api/workouts", function (req, res) {
+    db.Workout.find().then(dbresults => {
+        res.json(dbresults)
+    })
+    .catch(err => {
+    res.status(400).json(err);
+    });
 });
-router.post("/api/workouts",function(req,res){
-    
+//Create workout
+router.post("/api/workouts", function ({ body }, res) {
+    db.Workout.create(body).then(dbresults => {
+        res.json(dbresults);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+      });
 });
-router.put("/api/workouts/:id",function(req,res){
-    
+router.put("/api/workouts/:id", function (req, res) {
+
 });
-router.get("/api/workouts/range",function(req,res){
-    
+router.get("/api/workouts/range", function (req, res) {
+
 })
 
 module.exports = router
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,13 +3,14 @@ const router = express.Router();
 
 
 router.get("/exercise",function(req,res){
-
+    res.sendFile(path.join(__dirname,"../public/exercise.html"));
 });
+
 router.get("/stats",function(req,res){
-    
+    res.sendFile(path.join(__dirname,"../public/stats.html")); 
 });
 router.get("/",function(req,res){
-    res.sendFile()
-})
+    res.sendFile(path.join(__dirname,"../public/index.html"));
+});
 
 module.exports = router
